42ec9632b85891d2eeabc02fd83bfc27dbe9f741
Robert St. John <restjohn@users.noreply.github.com>
2022-12-05 12:22:18 -0700

    epub dao: remove return type from insert and add read-all method and ordering for queries

--- app/src/main/java/mil/nga/msi/datasource/electronicpublication/ElectronicPublicationDao.kt
+++ app/src/main/java/mil/nga/msi/datasource/electronicpublication/ElectronicPublicationDao.kt
@@ -10,14 +10,14 @@ interface ElectronicPublicationDao {
     suspend fun insert(x: ElectronicPublication)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(x: List<ElectronicPublication>): List<Long>
+    suspend fun insert(x: List<ElectronicPublication>)
 
-    @Update(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun update(x: ElectronicPublication): Int
+    @Query("select count(*) from epubs")
+    suspend fun count(): Int
 
-    @Query("SELECT COUNT(*) from epubs")
-    fun count(): Int
+    @Query("select * from epubs order by s3_key asc")
+    suspend fun readElectronicPublications(): List<ElectronicPublication>
 
-    @Query("SELECT * FROM epubs")
+    @Query("select * FROM epubs order by s3_key asc")
     fun observeElectronicPublications(): PagingSource<Int, ElectronicPublicationListItem>
 }
