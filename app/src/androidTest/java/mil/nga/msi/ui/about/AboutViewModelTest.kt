c63daccf5bb9847a17dc4fae2844e935b287d604
William Newman <newmanw10@gmail.com>
2023-06-05 13:50:47 -0600

    About viewmodel test

---
+++ app/src/androidTest/java/mil/nga/msi/ui/about/AboutViewModelTest.kt
@@ -0,0 +1,34 @@
+package mil.nga.msi.ui.about
+
+import io.mockk.coEvery
+import io.mockk.every
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.test.runTest
+import mil.nga.msi.repository.preferences.UserPreferencesRepository
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class AboutViewModelTest {
+
+   @Before
+   fun setup() {
+      MockitoAnnotations.openMocks(this)
+   }
+
+   @Test
+   fun testSetDeveloperMode() = runTest {
+      val mockRepository = mock<UserPreferencesRepository>()
+      Mockito.`when`(mockRepository.developer()).thenReturn(emptyFlow())
+
+      val viewModel = AboutViewModel(mockRepository)
+      viewModel.setDeveloperMode()
+
+      verify(mockRepository).setDeveloperMode()
+   }
+}
