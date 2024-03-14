5ae7824c3ee2d8ebd2d71503a486b3401ad1b0c3
JoshNels <139372369+JoshNels@users.noreply.github.com>
2024-03-14 11:34:08 -0500

    Remove unused import

--- app/src/main/java/mil/nga/msi/ui/modu/list/ModusScreen.kt
+++ app/src/main/java/mil/nga/msi/ui/modu/list/ModusScreen.kt
@@ -24,7 +24,6 @@ import mil.nga.msi.datasource.modu.Modu
 import mil.nga.msi.datasource.modu.ModuWithBookmark
 import mil.nga.msi.repository.bookmark.BookmarkKey
 import mil.nga.msi.ui.action.Action
-import mil.nga.msi.ui.action.AsamAction
 import mil.nga.msi.ui.main.TopBar
 import mil.nga.msi.ui.action.ModuAction
 import mil.nga.msi.ui.datasource.DataSourceActions
--- app/src/main/java/mil/nga/msi/ui/radiobeacon/list/RadioBeaconsScreen.kt
+++ app/src/main/java/mil/nga/msi/ui/radiobeacon/list/RadioBeaconsScreen.kt
@@ -24,7 +24,6 @@ import mil.nga.msi.datasource.radiobeacon.RadioBeacon
 import mil.nga.msi.datasource.radiobeacon.RadioBeaconWithBookmark
 import mil.nga.msi.repository.bookmark.BookmarkKey
 import mil.nga.msi.ui.action.Action
-import mil.nga.msi.ui.action.AsamAction
 import mil.nga.msi.ui.main.TopBar
 import mil.nga.msi.ui.action.RadioBeaconAction
 import mil.nga.msi.ui.datasource.DataSourceActions
