fd756bdd84a07e149bd87eceade52fd8f65f85c1
JoshNels <139372369+JoshNels@users.noreply.github.com>
2024-03-14 11:53:12 -0500

    Fix zoom from bookmarks tab (#18)

--- app/src/main/java/mil/nga/msi/ui/action/AsamAction.kt
+++ app/src/main/java/mil/nga/msi/ui/action/AsamAction.kt
@@ -18,7 +18,7 @@ sealed class AsamAction : Action() {
       }
    }
 
-   class Zoom(private val latLng: LatLng): Action() {
+   class Zoom(private val latLng: LatLng): AsamAction() {
       override fun navigate(navController: NavController) {
          val point = NavPoint(latLng.latitude, latLng.longitude)
          val encoded = Uri.encode(Json.encodeToString(point))
