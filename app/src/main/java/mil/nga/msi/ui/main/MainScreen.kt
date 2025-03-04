f5fcbbe5a13a0ee60fb6de8484b869dc2bc75c45
danielbenner <dan@vegafederal.com>
2025-03-04 09:49:41 -0600

    undid studio fix

--- app/src/main/java/mil/nga/msi/ui/main/MainScreen.kt
+++ app/src/main/java/mil/nga/msi/ui/main/MainScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.*
-//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.*
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
